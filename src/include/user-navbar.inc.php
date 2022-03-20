8167f94d13a8affa892ebb8cc666c62b6f77818a
Raven Lim <ravenlimzhexuan1@gmail.com>
2022-03-20 19:50:32 +0800

    Successfully Standardize user navbar for all user

--- src/include/user-navbar.inc.php
+++ src/include/user-navbar.inc.php
@@ -1,3 +1,29 @@
+<?php
+    if ($_SESSION == null){
+        session_start();
+    }
+    $type = $_SESSION['type'];
+
+    $supervisor_nav = [
+        "proposal.php" => "Proposal",
+        "project.php" => "Project",
+        "meeting.php" => "Meeting",
+        "marksheet.php" => "Marksheet",
+    ];
+
+    $student_nav = [
+
+    ];
+
+    $moderator_nav = [
+
+    ];
+
+    if ($type == "supervisor") $nav = $supervisor_nav;
+    elseif ($type = "student") $nav = $student_nav;
+    elseif ($type = "moderator") $nav = $moderator_nav;
+
+?>
 <div class="navbar">
         <div class="container">
             <a class="logo" href="index.php">FYP Planner</a>
@@ -5,10 +31,11 @@
                 <nav class="desktop-nav">
                     <ul>
                         <li><a class="nav-btn" href="index.php">Home</a></li>
-                        <li><a class="nav-btn" href="proposal.php">Proposal</a></li>
-                        <li><a class="nav-btn" href="project.php">Project</a></li>
-                        <li><a class="nav-btn" href="meeting.php">Meeting</a></li>
-                        <li><a class="nav-btn" href="marksheet.php">Marksheet</a></li>
+                        <?php
+                            foreach ($nav as $link => $title){
+                                echo("<li><a class='nav-btn' href='".$link."'>".$title."</a></li>");
+                            }
+                        ?>
                     </ul>
                 </nav>
 
@@ -28,10 +55,11 @@
                 <img id="mobile-exit" src="../assets/exit.svg">
                 <ul>
                     <li><a href="index.php">Home</a></li>
-                    <li><a href="proposal.php">Proposal</a></li>
-                    <li><a href="project.php">Project</a></li>
-                    <li><a href="meeting.php">Meeting</a></li>
-                    <li><a href="marksheet.php">Marksheet</a></li> 
+                    <?php
+                        foreach ($nav as $link => $title){
+                            echo("<li><a class='nav-btn' href='".$link."'>".$title."</a></li>");
+                        }
+                    ?>
                     <li><a href="profile.php">Profile</a></li>
                     <li><a href="../php/logout.php">Logout</a></li>
                 </ul>
@@ -46,10 +74,18 @@
         var currentIndex = 999;
 
         if(pageName == 'index.php' || pageName == '') currentIndex = 0;
-        else if(pageName == 'proposal.php') currentIndex = 1;
-        else if(pageName == 'project.php') currentIndex = 2;
-        else if(pageName == 'meeting.php') currentIndex = 3;
-        else if(pageName == 'marksheet.php') currentIndex = 4;
+        currentIndex = <?php
+            $i = 1;
+            $found = 0;
+            foreach ($nav as $link => $title){
+                if ($found == 0 && basename($_SERVER['REQUEST_URI'], '?' . $_SERVER['QUERY_STRING']) == $link){
+                    echo $i;
+                    $found = 1;
+                }
+                $i++;
+            }
+            $i--;
+        ?>
 
         $('.desktop-nav ul li').each(function(i) {
                 var index = $(this).index();
